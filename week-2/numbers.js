bd37881931120593c52a7e16ddd66694426a98ae
Davit Koch <datomadi555@gmail.com>
2026-01-26 15:43:02 -0500

    assignment numbers

---
+++ week-2/numbers.js
@@ -0,0 +1,85 @@
+// basica number operations
+
+console.log(1 + 2);
+console.log(2 - 1);
+console.log(2 * 1);
+console.log(2 / 1);
+console.log(2 % 1);
+
+// comperison true/false
+
+console.log(2 > 1);
+console.log(1 < 2);
+console.log(2 === 1);
+console.log(2 !== 1);
+
+//rounding numbers
+
+console.log(Math.round(3.6));
+console.log(Math.round(3.1));
+
+//rounding down
+
+console.log(Math.floor(3.7));
+
+//rounding up
+
+console.log(Math.ceil(3.2));
+
+// rounding decimal
+
+console.log(Math.trunc(3.7));
+
+//formating numbers
+
+let number = 3.456;
+console.log(number.toFixed(2));
+
+//converting number
+console.log(Number("30"));
+console.log(Number("davit"));
+
+// converting numbers >> integer
+
+console.log(parseInt("3.1"));
+
+//converting numbers >> decimal numer
+
+console.log(parseFloat("10.5"));
+
+//checking numbers
+
+console.log(isNaN("dato"));
+console.log(isNaN(36));
+
+//checking numbers
+
+console.log(Number.isInteger(5));
+console.log(Number.isInteger(4.5));
+
+//absolute number
+
+console.log(Math.abs(-1));
+
+//math utility Methods>> smallets numver
+
+console.log(Math.min(3, 10, 5));
+
+////math utility Methods>> largext numver
+
+//math utility Methods>> smallets numver
+
+console.log(Math.max(10, 20, 30));
+
+//raises number to a power
+
+console.log(Math.pow(2, 3));
+
+//returns the square rooot of a number
+
+console.log(Math.sqrt(25));
+
+//generateing a random decimal number
+
+console.log(Math.random());
+console.log(Math.random() * (200 - 300 + 1)) + 200;
