9416e896121b651e4b74d9ca771c343d8981d0a5
Davit Koch <datomadi555@gmail.com>
2026-01-26 14:23:38 -0500

    assignmnt string

--- week-2/string.js
+++ week-2/string.js
@@ -1,34 +1,88 @@
-// lower case upper case
+//1, string length
 
-let greeting = "Hello World";
+let text = "kochiashvili";
+console.log(text.length);
 
-console.log(greeting.toUpperCase());
-console.log(greeting.toLowerCase());
+//2.accesing characters
 
-//     .trim()  removes extra space
+console.log(text[0]);
+console.log(text[4]);
 
-let space = "   test ";
-console.log(space.trim());
+//3.changing case
 
-////  .slice (start, and)
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
 
-let myName = "kochiashvili";
+//4.trimming spaces
 
-console.log(myName.slice(0, 4));
+text = "   kochiashvili  ";
+console.log(text.trim());
 
-// replace
+//5.slice(start,end)
 
-///   .replace()
+text = "kochiashvili";
+console.log(text.slice(0, 4));
+console.log(text.slice(4));
 
-let text = "i like cats";
+//6. replace (searchValue, newValue)
 
-console.log(text.replace("cats", "dogs"));
+let newText = "David enjoyes coding";
+console.log(newText.replace("enjoyes", "likes"));
 
-// .includes
+//7.includes (substring)
 
-let file = "report.pdf";
-console.log(file.startsWith("report"));
-console.log(file.endsWith(".pdf"));
+console.log(newText.includes("coding"));
+console.log(newText.includes("driving"));
 
-const studentName = "Tamara";
-console.log(`${studentName} loves coding`);
+//8.split (separator)
+
+let string = "veni, vidi, vici";
+
+console.log(string.split(","));
+console.log(string.split(""));
+console.log(string.split(" "));
+
+//9.concat(string1,string2...)
+
+console.log(string.concat(" ", text));
+
+//10.indexOf (substring)
+
+console.log(string.indexOf("veni"));
+console.log(string.indexOf("alpha"));
+
+//11.startswith() and endswith()
+
+console.log(string.startsWith("veni"));
+console.log(string.endsWith("vici"));
+
+//template literals
+
+let quote = `${text}, ${string}`;
+
+console.log(quote);
+
+//multi-line strings
+
+let attantion = `
+
+    warning
+    wanted alive or dead
+    dangerous
+    `;
+
+console.log(attantion);
+
+//expressions inside taemplate literals
+
+let aa = "apple";
+let bb = "cinnamon";
+let appplePie = aa + bb;
+console.log(`you put together ${aa} and ${bb} you will get ${appplePie}`);
+
+//combining string method + template literals'
+
+let david = "good";
+let age = 36;
+
+console.log(`david is ${david.toUpperCase()} he is ${age}`);
